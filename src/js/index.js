b343a95494f765c2a295a255bd449d71ebdce146
Natsuel Menezes <natsuel_santos@hotmail.com>
2025-10-21 20:37:50 -0300

    Refatoração de código

--- src/js/index.js
+++ src/js/index.js
@@ -1,38 +1,24 @@
-document.getElementById('btn-enviar').addEventListener('click', function() {
-    const campos = [
-        { id: 'name', nome: 'Nome completo'},
-        { id: 'email', nome: 'Email'},
-        { id: 'tel', nome: 'Telefone'},
-        { id: 'msg', nome: 'Mensagem'},
-    ]
+camposFormulario = document.querySelectorAll('.campo');
 
-    campos.forEach(campo => {
-        const input = document.getElementById(campo.id)
+document.getElementById('btn-enviar').addEventListener('click', (e) => {
+    e.preventDefault();
+    camposFormulario.forEach(campo => {
 
-        const alertaAntigo = input.nextSibling;
-        
-        if (alertaAntigo && alertaAntigo.classList && alertaAntigo.classList.contains('alerta')) {
-        alertaAntigo.remove();
+        const verificarAlerta = campo.nextElementSibling;
+
+        if (verificarAlerta && verificarAlerta.classList.contains('alerta')) {
+        verificarAlerta.remove();
         }
 
-        if (!input.value.trim()) {
+        if(!campo.value.trim()) {
             const alerta = document.createElement('span');
-            alerta.textContent = 'Campo obrigatório';
             alerta.classList.add('alerta');
-            input.parentNode.insertBefore(alerta, input.nextSibling);
-
-            input.style.borderColor = 'red';
+            alerta.textContent = 'Este campo é obrigatório.';
+            campo.parentNode.insertBefore(alerta, campo.nextSibling);
+            campo.style.borderColor =  '#ff4d4d'
         }
-
         else {
-            input.style.borderColor =  '#8ff981ff';
+            campo.style.borderColor =  '#8ff981ff';
         }
     })
 })
-
-window.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('name').value = '';
-    document.getElementById('email').value = '';
-    document.getElementById('tel').value = '';
-    document.getElementById('msg').value = '';
-});
